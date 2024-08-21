aac495092f658a808d700e88b590ca57c58d54c7
Worawut Khumnoi <6431501102@lamduan.mfu.ac.th>
2024-08-21 15:03:59 +0700

    Update db.py

--- Project/config/db.py
+++ Project/config/db.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from pathlib import Path
 
 # Load environment variables from .env file
-pathenv = Path('/app/.env')
+pathenv = Path('../.env')
 load_dotenv(dotenv_path=pathenv)
 
 # SNMP and MongoDB configuration
